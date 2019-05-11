de3ac413cee4574b5c6e9ce0eb1bd44d7ec6b39f
tacotime11 <coryrichardschmidt@gmail.com>
2019-05-11 20:47:30 +0200

    Fixed the document conditional

--- js/scripts.js
+++ js/scripts.js
@@ -17,5 +17,5 @@ for (var i = 0; i < repository.length; i++) {
   document.write('Bulbasaur' Height: 0.7);
   document.write('Wigglyfuff' Height: 1);
   document.write('Butterfree' Height: 1.1);
-  if length > 1.1 document.write(Wow, that’s big!)
+  if (length > 1.1 document.write 'Wow, that’s big!'')
 }

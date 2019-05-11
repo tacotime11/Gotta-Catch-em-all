c066d4d207c2589ff280f45d47ba6fe41b9b7d62
tacotime11 <coryrichardschmidt@gmail.com>
2019-05-11 20:43:28 +0200

    Added an Array with Pokemon characters with a loop and conditional

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,21 @@
-alert('Hello world');
+var repository = [
+  ['Bulbasaur',
+    0.7,
+    ['grass', 'poison'],
+  ]
+  ['Wigglyfuff',
+    1,
+    ['fairy', 'normal'],
+  ]
+  ['Butterfree',
+    1.1,
+    ['bug', 'flying'],
+  ]
+]
+
+for (var i = 0; i < repository.length; i++) {
+  document.write('Bulbasaur' Height: 0.7);
+  document.write('Wigglyfuff' Height: 1);
+  document.write('Butterfree' Height: 1.1);
+  if length > 1.1 document.write(Wow, that’s big!)
+}

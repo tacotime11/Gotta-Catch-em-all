12c8ba4604307648bf4c451c85ece75010257624
tacotime11 <coryrichardschmidt@gmail.com>
2019-05-18 20:34:46 +0200

    added in the other objects

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,8 @@ var repository = [
 
 for (var i = 0; i < repository.length; i++) {
   document.write ('<p>' + repository[i].name + '</p>');
+  document.write ('<p>' + repository[i].height + '</p>');
+  document.write ('<p>' + repository[i].type + '</p>');
   if (repository[i].height >  1) {
     document.write ('<p>' + 'Wow, that’s big!' + '</p>')
   }

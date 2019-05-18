6806b0748d5aa832ee2efdb50c25f8bb6029c2e7
tacotime11 <coryrichardschmidt@gmail.com>
2019-05-18 20:53:31 +0200

    switched the bracket again

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,7 @@ for (var i = 0; i < repository.length; i++) {
   document.write ('<h1>' + repository[i].name + '</h1>');
   document.write ('<h2>' + repository[i].height + '</h2>');
   document.write ('<h3>' + repository[i].type + '</h3>');
+  }
   if (repository[i].height =  1.1) {
     document.write ('<p>' + 'Wow, that’s big!' + '</p>')
   }
-}

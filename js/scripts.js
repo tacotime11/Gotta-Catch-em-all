d1078e888f6e381959da1425149b8b7c1c510422
tacotime11 <coryrichardschmidt@gmail.com>
2019-05-25 21:35:08 +0200

    changed to forEach functions

--- js/scripts.js
+++ js/scripts.js
@@ -12,11 +12,7 @@ var repository = [
     type: ['bug', 'flying'], },
 ]
 
-for (var i = 0; i < repository.length; i++) {
-  document.write ('<h1>' + repository[i].name + '</h1>');
-  document.write ('<h2>' + repository[i].height + '</h2>');
-  document.write ('<h3>' + repository[i].type + '</h3>');
-  if (repository[i].height >  1) {
-    document.write ('<p>' + 'Wow, that’s big!' + '</p>')
-  }
-}
+document.write('<h3>Pokemon</h3>');
+repository.forEach(function(currentItem){
+  document.write('<p>' + currentItem + '</p>');
+});
